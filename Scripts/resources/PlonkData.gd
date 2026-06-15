61fca1a376864a2c00f8766772b494c55b8a4bc4
Steven R. Xie <48636116+khiro-ptera@users.noreply.github.com>
2026-06-15 13:27:45 -0700

    assets readd

--- Scripts/resources/PlonkData.gd
+++ Scripts/resources/PlonkData.gd
@@ -3,14 +3,21 @@ class_name PlonkData extends Resource
 @export var id: String = ""
 @export var display_name: String = ""
 @export var base_currency_per_bounce: float = 1.0
-@export var speed: float = 200.0
-@export var radius: float = 16.0
-@export var sell_value: float = 0.0
+@export var cost: float = 10.0
 @export var scene: PackedScene
-
-# ex. { "type": "currency", "amount": 500 }
-# or { "type": "stat", "stat": "total_bounces", "amount": 100 }
 @export var unlock_condition: Dictionary = {}
-
-# ex. ["GravityComponent", "SuckComponent"]
 @export var components: Array[String] = []
+
+@export var mass: float = 1.0
+@export var physics_material: PhysicsMaterial
+@export var linear_speed: float = 200.0
+@export var max_angular_velocity: float = 10.0
+@export var moment_of_inertia_scale: float = 1.0  
+
+@export_enum("Circle", "Star", "Custom") var shape_type: String = "Circle"
+@export var radius: float = 16.0
+
+# unlock ex. { "type": "currency", "amount": 500 }
+# or { "type": "stat", "stat": "total_bounces", "amount": 100 }
+
+# compontnents ex. ["GravityComponent", "SuckComponent"]
